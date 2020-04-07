72e040bdbd65764fd21dd6a9f35528eec711bd20
msun4100 <msun4100@gmail.com>
2020-04-07 20:42:09 +0900

    #3.13 Computed Fields in Prisma

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -7,6 +7,15 @@ export default {
       const { email, secret } = args;
       const user = await prisma.user({ email });
       if (user.loginSecret === secret) {
+        // 시크릿 확인되면 삭제
+        await prisma.updateUser({
+          where: {
+            id: user.id,
+          },
+          data: {
+            loginSecret: "",
+          },
+        });
         return generateToken(user.id); // jwt token
       } else {
         throw Error("Wrong email/secret combination");
--- src/server.js
+++ src/server.js
@@ -4,13 +4,14 @@ import logger from "morgan";
 import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
+import { isAuthenticated } from "./middlewares";
 
 const PORT = process.env.PORT || 4000;
 
 // context에 request만 담긴 객체 리턴, console.dir로 확인
 const server = new GraphQLServer({
   schema,
-  context: ({ request }) => ({ request }),
+  context: ({ request }) => ({ request, isAuthenticated }),
 });
 
 server.express.use(logger("dev"));
