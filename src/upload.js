3bda6f4c8862d23e2c1cb23db50541b769ccdc77
msun4100 <msun4100@gmail.com>
2020-05-11 16:56:30 +0900

    #18.9 Uploading to S3

--- src/upload.js
+++ src/upload.js
@@ -1,27 +1,35 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 import path from "path";
 
-// const upload = multer({ dest: "uploads/" });
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "ap-northeast-2",
+});
+
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, "uploads");
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "prismagram-msun4100",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
     },
-    filename(req, file, done) {
+    key: function(req, file, cb) {
       const ext = path.extname(file.originalname);
       const basename = path.basename(file.originalname, ext);
-      done(null, basename + new Date().valueOf() + ext);
+      cb(null, basename + new Date().valueOf() + ext);
     },
   }),
-  limits: { fileSize: 20 * 1024 * 1024 },
 });
 
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   const {
-    file: { path },
+    file: { location },
   } = req;
-  console.log(req.file);
-  res.json({ path });
+  res.json({ location });
 };
